d65a4e26e23b954976519b84c33ba023586b217a
Chidiebere Okpala <okpalacollins4@gmail.com>
2020-04-14 04:59:17 -0600

    Initial commit - Starter code

---
+++ app/src/main/java/com/example/android/sandwichclub/utils/JsonUtils.java
@@ -0,0 +1,11 @@
+package com.example.android.sandwichclub.utils;
+
+import com.example.android.sandwichclub.model.Sandwich;
+
+public class JsonUtils {
+
+    public static Sandwich parseSandwichJson(String json) {
+        return null;
+    }
+
+}
